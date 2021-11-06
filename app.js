485a06f799cd2219141cbb393148ff88b28460d8
koustav2001 <89209950+koustav2001@users.noreply.github.com>
2021-11-06 13:29:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+//nav design
+let nav = document.querySelector('.navigation-wrap');
+window.onscroll=function()
+{
+    if(document.documentElement.scrollTop >20)
+    {
+        nav.classList.add("scroll-on");
+    }
+    else
+    {
+        nav.classList.remove("scroll-on");
+    }
+}
+
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse');
+navBar.forEach((a)=>
+{
+    a.addEventListener("click",()=>
+    {
+        navCollapse.classList.remove("show");
+    })
+})
+
 //counter code
 document.addEventListener("DOMContentLoaded",()=>
 {
